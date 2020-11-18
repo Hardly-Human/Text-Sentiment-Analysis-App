a79cd1b7f4d4e225bfafd4f2e993aeead8c64846
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2020-11-18 10:38:32 +0530

    Added footer

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import streamlit as st
+import requests
 
+
+def footer():
+	st.markdown("""
+	* * *
+	Built with ❤️ by [Rehan uddin](https://hardly-human.github.io/)
+	""")
+	st.success("Rehan uddin (Hardly-Human)👋😉")
+	st.markdown("### [Give Feedback](https://www.iamrehan.me/forms/feedback_form/feedback_form.html)\
+	 `            `[Report an Issue](https://www.iamrehan.me/forms/report_issue/report_issue.html)")
 
 
 def main():
   
 	st.title("Sentiment Analysis App")
 	st.text("Built with Python and Streamlit")
-	st.markdown("### [![Open Source Love svg1](https://aleen42.github.io/badges/src/github.svg)](https://github.com/Hardly-Human/Image-Captioning-App)\
+	st.markdown("### [![Open Source Love svg1](https://aleen42.github.io/badges/src/github.svg)](https://github.com/Hardly-Human/Text-Sentiment-Analysis-App)\
 	`            `[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://lbesson.mit-license.org/)")
 
 	input_text = st.text_area("Enter Your text here", height = 50)
@@ -35,3 +45,4 @@ def main():
 
 if __name__== "__main__":
 	main()
+	footer()
